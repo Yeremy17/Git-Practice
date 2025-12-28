3862c4f8a842d5c9846f0f6b771aee0b1d1d5f2f
Yeremy Acuña <182255189+Yeremy17@users.noreply.github.com>
2025-12-28 14:16:36 -0500

    borrar este commit.

---
+++ eliminargit.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello worlddddddddd\n";
+  
+    return 0;
+}
--- practicandogit.cpp
+++ practicandogit.cpp
@@ -7,5 +7,6 @@ int main()
 {
     cout << "Hello Git! \n";
     cout << "My name is Yeremy :)";
+    cout << "Borrar esto please. " ;
     return 0;
 }
