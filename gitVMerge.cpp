8da2d92792109ea37812c13bc03f19269d80b825
Yeremy Acuña <182255189+Yeremy17@users.noreply.github.com>
2025-12-29 22:38:24 -0500

    Git stash, git stash list, git stash pop HECHO

--- gitVMerge.cpp
+++ gitVMerge.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Hello world merge in add continue commits\n";
-  
+    cout << "Git merge añadido";
+    cout << "Git stash hecho, commit temporal";
     return 0;
 }
