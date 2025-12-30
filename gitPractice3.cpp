5c34f823e41a6e0b8eb19b8c6d2c641305413132
Yeremy Acuña <182255189+Yeremy17@users.noreply.github.com>
2025-12-29 21:19:17 -0500

    Añadido feature/ o

---
+++ gitPractice3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    int edad; 
+    cout << "Versiono rama feature/practice";
+    system("pause>0");
+    return 0;
+}
