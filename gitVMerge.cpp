2d034b526a67e28d62cda5a697658fa5ed9077c6
Yeremy Acuña <182255189+Yeremy17@users.noreply.github.com>
2026-01-02 21:54:36 -0500

    2/01/2026

--- gitVMerge.cpp
+++ gitVMerge.cpp
@@ -6,7 +6,8 @@ using namespace std;
 int main()
 {
     cout << "Hello world merge in add continue commits\n";
-    cout << "Git merge añadido";
-    cout << "Git stash hecho, commit temporal";
+    cout << "Git merge añadido" << endl;
+    cout << "Git stash hecho, commit temporal" << endl;
+    cout << "Git hub: Yeremy Acuña Chavez" << endl;
     return 0;
 }
